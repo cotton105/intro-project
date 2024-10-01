75e735d70382f03651f9263a4353f71672d1e389
Luke Cotton <luke@cotton.org.uk>
2024-10-01 19:37:16 +0100

    Allow database path configuration via env variables In order of priority: 1. `DB_PATH` env variable. 2. `NODE_ENV` is "*TEST*" - will use a default testing database. 3. Default to the main database.

--- config.js
+++ config.js
@@ -1,7 +1,16 @@
 const path = require( "path" )
 
+let dbpath
+if( undefined !== process.env.DB_PATH ) {
+  dbpath = path.resolve( __dirname, process.env.DB_PATH )
+} else if( "TEST" === process.env.NODE_ENV ) {
+  dbpath = path.resolve( __dirname, "db", "database.test.db" )
+} else {
+  dbpath = path.resolve( __dirname, "db", "database.db" )
+}
+
 const config = {
-  dbpath: process.env.DB_PATH ? path.resolve( __dirname, process.env.DB_PATH ) : path.resolve( __dirname,  "db", "database.db" ),
+  dbpath
 }
 
 module.exports = config
