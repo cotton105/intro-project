f3c99c6d1a4b510828d52fb1ebbd0d60111612db
Luke Cotton <luke@cotton.org.uk>
2024-10-02 18:04:05 +0100

    Implement frontend test for table to display People correctly

--- test/frontend.test.js
+++ test/frontend.test.js
@@ -5,6 +5,7 @@ const firefox = require( "selenium-webdriver/firefox" )
 const { Builder, Browser, By, Key, until } = require( "selenium-webdriver" )
 
 const config = require( "../config" )
+const homepageurl = `http://localhost:${config.port}`
 
 let driver
 
@@ -16,15 +17,29 @@ describe( "Frontend tests", async function () {
       .build()
   } )
 
-  it( "web page should be accessible", async function () {
-    await driver.get( `http://localhost:${config.port}` )
+  it( "should correctly display the web page", async function () {
+    await driver.get( homepageurl )
     const expectedtitle = "Work!"
     const pagetitle = await driver.getTitle()
     assert( expectedtitle === pagetitle, `Page title was "${pagetitle}" - expected "${expectedtitle}".` )
   } )
 
   it( "should correctly display the users in the database on the table", async function () {
-    throw new Error( "Unimplemented test." )
+    const getresponse = await fetch( `${homepageurl}/api/people` )
+    if( !getresponse.ok ) {
+      throw new Error( getresponse )
+    }
+    const apipeople = await getresponse.json()
+    await driver.get( homepageurl )
+    const table = await driver.findElement( By.id( "peopletable" ) )
+    const tablerows = await table.findElements( By.css( "tbody > tr" ) )
+    assert.equal( apipeople.length, tablerows.length )
+    for( let i = 0; i < apipeople.length; i++ ) {
+      const apipersonname = apipeople[i].name
+      const rowcolumns = await tablerows[i].findElements( By.css( "td" ) )
+      const tablepersonname = await rowcolumns[0].getText()
+      assert.equal( apipersonname, tablepersonname )
+    }
   } )
 
   after( async function () {
