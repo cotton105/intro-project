7644d6c0c97dcd967ae857a57332b4e147acc0dc
Luke Cotton <luke@cotton.org.uk>
2024-10-03 19:00:29 +0100

    Reduce code repetition with new frontend API handler

--- public/js/api.js
+++ public/js/api.js
@@ -3,25 +3,12 @@
 const rooturl = `${window.location.protocol}//${window.location.host}/api/`
 
 /**
- * Wrapper for all API GET requests
+ * Wrapper for all API GET requests.
  * @param { string } api 
  * @returns { Promise< object > }
  */
 export async function getdata( api ) {
-  try {
-    const url = rooturl + api
-
-    const response = await fetch( url )
-
-    if( response.ok ) {
-      const data = await response.json()
-      return data
-    } else {
-      throw new Error( `Request failed with status: ${response.status}` )
-    }
-  } catch ( error ) {
-    console.error( 'Error fetching data:', error.message )
-  }
+  return await makerequest( api, "GET" )
 }
 
 /**
@@ -31,47 +18,43 @@ export async function getdata( api ) {
  * @returns { Promise }
  */
 export async function putdata( api, data ) {
-  try {
-    const url = rooturl + api
-    const request = {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify( data )
-    }
-
-    const response = await fetch( url , request )
-
-    if( !response.ok ) {
-      throw new Error( `Request failed with status: ${response.status}` )
-    }
-  } catch ( error ) {
-    console.error( 'Error fetching data:', error.message )
-  }
+  return await makerequest( api, "PUT", data )
 }
 
 /**
- *  * Wrapper for all API DELETE requests.
+ * Wrapper for all API DELETE requests.
  * @param { string } api
  * @param { object } data
  * @returns { Promise }
  */
 export async function deletedata( api, data ) {
+  return await makerequest( api, "DELETE", data )
+}
+
+/**
+ * Handle API requests.
+ * @param { string } api 
+ * @param { string } method 
+ * @param { object } data 
+ * @returns { Promise }
+ */
+async function makerequest( api, method, data ) {
   try {
     const url = rooturl + api
-    const request = {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify( data )
+    const request = { method: method }
+    if( undefined !== data ) {
+      request.headers = { "Content-Type": "application/json" }
+      request.body = JSON.stringify( data )
     }
 
     const response = await fetch( url , request )
 
     if( !response.ok ) {
       throw new Error( `Request failed with status: ${response.status}` )
+    }
+    const retrieveddata = await response.json()
+    if( undefined !== retrieveddata ) {
+      return retrieveddata
     }
   } catch ( error ) {
     console.error( 'Error fetching data:', error.message )
